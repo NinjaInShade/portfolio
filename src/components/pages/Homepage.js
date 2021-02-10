8590df327354664f5a4d972431b9b1e0a6e5ce74
Leon Michalak <leonmichalak6@gmail.com>
2021-02-10 17:50:48 +0000

    CSS Fixes

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -7,17 +7,19 @@ import Button from "../../components/UI/Button";
 export default function Homepage() {
   return (
     <section className="homepage">
-      <h1>
-        Hi, I'm <span>Leon</span>
-      </h1>
-      <p className="homepage-role">UK based web developer</p>
-      <img src={avatar} alt="Avatar of me" />
-      <Button>
-        <a href="/projects">My projects</a>
-      </Button>
-      <a className="homepage-cv" href="/">
-        Download cv
-      </a>
+      <div class="homepage-header">
+        <h1>
+          Hi, I'm <span>Leon</span>
+        </h1>
+        <p className="homepage-role">UK based web developer</p>
+        <img src={avatar} alt="Avatar of me" />
+        <Button>
+          <a href="/projects">My projects</a>
+        </Button>
+        <a className="homepage-cv" href="/">
+          Download cv
+        </a>
+      </div>
 
       <div className="homepage-find-out-more">
         <p>Check more out</p>
