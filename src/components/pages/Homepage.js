7dc7a46e73ec648293399e6a6806d2e1f3766904
Leon Michalak <leonmichalak6@gmail.com>
2021-02-10 18:09:42 +0000

    Changed one element from absolute and re configured adjacent elements to make it work

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -6,25 +6,28 @@ import Button from "../../components/UI/Button";
 
 export default function Homepage() {
   return (
-    <section className="homepage">
-      <div class="homepage-header">
-        <h1>
-          Hi, I'm <span>Leon</span>
-        </h1>
-        <p className="homepage-role">UK based web developer</p>
-        <img src={avatar} alt="Avatar of me" />
-        <Button>
-          <a href="/projects">My projects</a>
-        </Button>
-        <a className="homepage-cv" href="/">
-          Download cv
-        </a>
-      </div>
+    <>
+      <section className="homepage">
+        <div class="homepage-header">
+          <h1>
+            Hi, I'm <span>Leon</span>
+          </h1>
+          <p className="homepage-role">UK based web developer</p>
+          <img src={avatar} alt="Avatar of me" />
+          <Button>
+            <a href="/projects">My projects</a>
+          </Button>
+          <a className="homepage-cv" href="/">
+            Download cv
+          </a>
+        </div>
 
-      <div className="homepage-find-out-more">
-        <p>Check more out</p>
-        <i class="fas fa-chevron-down"></i>
-      </div>
-    </section>
+        <div className="homepage-find-out-more">
+          <p>Check more out</p>
+          <i class="fas fa-chevron-down"></i>
+        </div>
+      </section>
+      <section className="homepage-test"></section>
+    </>
   );
 }
