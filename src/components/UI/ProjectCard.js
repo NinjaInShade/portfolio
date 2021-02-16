85d229618b350201f8076868b06aa7bfc6baa521
Leon Michalak <leonmichalak6@gmail.com>
2021-02-16 23:39:48 +0000

    Added all preview projects

--- src/components/UI/ProjectCard.js
+++ src/components/UI/ProjectCard.js
@@ -5,25 +5,30 @@ import "./ProjectCard.css";
 
 export default function ProjectCard({ img, title, description, live_URL, github_URL, reversed, white }) {
   return (
-    <div className="project-card flex" style={reversed ? { flexDirection: "row-reverse" } : {}}>
+    <div
+      className={white ? "project-card-white flex" : "project-card flex"}
+      style={{
+        flexDirection: reversed ? "row-reverse" : "",
+      }}
+    >
       <img src={img} alt="Project thumbnail" />
       <div className="project-card-text flex">
         <div className="project-card-text-title flex">
           <a href={github_URL} target="_blank" rel="noreferrer">
-            <i className="fab fa-github"></i>
+            <i className="fab fa-github" style={white ? { color: "#2E1B6F" } : {}}></i>
           </a>
-          <h2>{title}</h2>
+          <h2 style={white ? { color: "#2E1B6F" } : {}}>{title}</h2>
         </div>
-        <p>{description}</p>
+        <p style={white ? { color: "#5B46B8" } : {}}>{description}</p>
         <Button>
           <a href={live_URL} target="_blank" rel="noreferrer">
             View live site
           </a>
         </Button>
       </div>
-      <div className="project-card-bg"></div>
-      <div className="project-card-wave"></div>
-      <div className="project-card-wave-2"></div>
+      <div className="project-card-bg" style={white ? { display: "none" } : {}}></div>
+      <div className="project-card-wave" style={white ? { display: "none" } : {}}></div>
+      <div className="project-card-wave-2" style={white ? { display: "none" } : {}}></div>
     </div>
   );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,6 +4,9 @@ import Astronaught from "../../assets/images/astronaught.svg";
 import Button from "../UI/Button";
 import ProjectCard from "../UI/ProjectCard";
 import FlashcardsThumbnail from "../../assets/thumbnails/FlashcardsThumbnail.png";
+import SiteBuilderThumbnail from "../../assets/thumbnails/SiteBuilderThumbnail.png";
+import TodoThumbnail from "../../assets/thumbnails/TodoThumbnail.png";
+import HubThumbnail from "../../assets/thumbnails/HubThumbnail.png";
 
 import "./Home.css";
 import "./Home-projects.css";
@@ -38,8 +41,33 @@ export default function Home() {
           img={FlashcardsThumbnail}
           title="Flashcards app"
           description="This app is designed to help with revision by letting you make flashcards which you can flip to reveal the answer. You can group flashcard into a collection and even test yourself."
-          live_URL="/"
+          live_URL="https://leonsflashcards.web.app/"
           github_URL="https://github.com/NinjaInShade/flashcards"
+        />
+        <ProjectCard
+          img={SiteBuilderThumbnail}
+          title="Site builder app"
+          description="This app’s idea is to help people with making a polished website through providing the user with resources, tips and things that should be included to provide a good user experience with strong UI."
+          live_URL="https://sitebuilder-66d12.web.app/"
+          github_URL="https://github.com/NinjaInShade/site-builder"
+          reversed
+          white
+        />
+        <ProjectCard
+          img={TodoThumbnail}
+          title="Todo app"
+          description="Most web developers have made this project as it encorporates key but simple concepts for a beginner developer to learn. I decided to add a dark/light theme to it to make it a bit less bland."
+          live_URL="https://todoapp-48ccc.web.app/"
+          github_URL="https://github.com/NinjaInShade/todo-app"
+        />
+        <ProjectCard
+          img={HubThumbnail}
+          title="Personal hub app"
+          description="I made this for myself and included some useful tools like links to my site builder app and todo app, code docs and experimented with a settings page where you can customize bits of the app."
+          live_URL="https://leonshub-ec26e.web.app/"
+          github_URL="https://github.com/NinjaInShade/leons_hub"
+          reversed
+          white
         />
       </section>
     </>
