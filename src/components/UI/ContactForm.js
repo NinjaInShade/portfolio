77f083f148efbfe6bdd8139669d2bd45e72ecb3c
Leon Michalak <leonmichalak6@gmail.com>
2021-02-17 19:01:10 +0000

    Send mail icon changed

--- src/components/UI/ContactForm.js
+++ src/components/UI/ContactForm.js
@@ -61,7 +61,18 @@ export default function ContactForm() {
             />
           </div>
           <Button className="flex" type="submit" onClick={(e) => submitForm(e)}>
-            <i className="fas fa-phone-alt"></i>
+            {/* <i className="fas fa-phone-alt"></i> */}
+            <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M29.1668 5.83325H8.75016C7.14162 5.83325 5.8335 7.14138 5.8335 8.74992V16.0416H8.75016V11.6666L18.0835 18.6666C18.336 18.8557 18.643 18.9579 18.9585 18.9579C19.274 18.9579 19.581 18.8557 19.8335 18.6666L29.1668 11.6666V24.7916H17.5002V27.7083H29.1668C30.7754 27.7083 32.0835 26.4001 32.0835 24.7916V8.74992C32.0835 7.14138 30.7754 5.83325 29.1668 5.83325ZM18.9585 15.677L9.72141 8.74992H28.1956L18.9585 15.677Z"
+                fill="white"
+              />
+              <path
+                d="M2.9165 17.5H13.1248V20.4167H2.9165V17.5ZM5.83317 21.875H14.5832V24.7917H5.83317V21.875ZM10.2082 26.25H16.0415V29.1667H10.2082V26.25Z"
+                fill="white"
+              />
+            </svg>
+
             <p>Send mail</p>
           </Button>
         </form>
