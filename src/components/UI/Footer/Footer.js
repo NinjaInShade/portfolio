2c6da8db9037e9738195648ab98b252449af0320
Leon Michalak <leonmichalak6@gmail.com>
2021-02-18 23:32:27 +0000

    Navbar sidebar overlay fade out opacity fix

--- src/components/UI/Footer/Footer.js
+++ src/components/UI/Footer/Footer.js
@@ -1,5 +1,27 @@
 import React from "react";
 
+import "./Footer.css";
+
 export default function Footer() {
-  return <div></div>;
+  return (
+    <footer>
+      <div className="container flex">
+        <div>
+          <h3>Credit</h3>
+          <ul>
+            <li>https://bgjar.com</li>
+            <li>https://fontawesome.com</li>
+            <li>© Leon Michalak 2021</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Contact</h3>
+        </div>
+        <div>
+          <h3>Media</h3>
+        </div>
+      </div>
+      <div className="bg"></div>
+    </footer>
+  );
 }
