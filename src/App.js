2f474ec99138b7eb25f7da47efcbb5cc63bde090
Leon Michalak <leonmichalak6@gmail.com>
2021-02-22 20:43:02 +0000

    User is given feedback on process of sending the email from form. Button text changes from send mail to sending mail... to mail recieved

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import Navbar from "./components/UI/Navbar/Navbar";
 import Footer from "./components/UI/Footer/Footer";
 import Unmatched from "./components/pages/Unmatched/Unmatched";
 import Construction from "./components/pages/Construction/Construction";
-import EmailConfirmation from "./components/pages/EmailConfirmation/EmailConfirmation";
 import Home from "./components/pages/Home/Home.js";
 
 import "./App.css";
 
 // TODO: Refactor contact form. The html/css may be excessive and could be a lot cleaner and smaller.
-// TODO: Add loading spinner on contact form after req is sent. Make this while user waits to be redirected to email page
+// TODO: Add loading status on contact form after req is sent. Make this while user waits to be redirected to email page
 // TODO: 404 error page
 // TODO: Go through and make sure all of the sites components and such include semantic html.
 // TODO: Make astronaught on home page come shooting up from near bottom right corner into place on page load.
@@ -28,9 +27,6 @@ function App() {
         </Route>
         <Route path="/projects" exact>
           <Construction />
-        </Route>
-        <Route path="/email" exact>
-          <EmailConfirmation />
         </Route>
         <Route>
           <Unmatched />
--- src/components/UI/ContactForm/ContactForm.js
+++ src/components/UI/ContactForm/ContactForm.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
 import Button from "../Button/Button";
 import { sanitizeData, minMaxLength, isEmail } from "../../../util/validation";
 
 import "./ContactForm.css";
 
 export default function ContactForm() {
-  const [emailSent, setEmailSent] = useState({ successfull: undefined, sent: false });
+  const [emailStatus, setEmailStatus] = useState({ recieved_response: undefined, succcessfull: false });
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
@@ -64,6 +63,8 @@ export default function ContactForm() {
       setEmail("");
       setMessage("");
 
+      setEmailStatus({ recieved_response: false });
+
       // Send fetch req to backend
       fetch(`${process.env.REACT_APP_SEND_EMAIL_DOMAIN}`, {
         method: "POST",
@@ -81,124 +82,113 @@ export default function ContactForm() {
           response.json();
         })
         .then((data) => {
-          console.log("send successfully");
-          return setEmailSent({ sent: true, successfull: true });
+          return setEmailStatus({ recieved_response: true, successfull: true });
         })
         .catch((error) => {
-          console.error("Error:", error);
+          console.log("Error occurred", error);
 
-          return setEmailSent({ sent: true, successfull: false });
+          return setEmailStatus({ recieved_response: true, successfull: false });
         });
     } else {
       return;
     }
   }
 
-  if (emailSent.sent) {
-    return (
-      <Redirect
-        to={{
-          pathname: "/email",
-          state: { status: { successfull: emailSent.successfull } },
-        }}
-      />
-    );
-  }
-
   return (
-    <section className="home-contact flex" id="contact">
-      <div className="home-contact-container flex">
-        <div className="home-contact-text flex">
-          <h2
-            className="home-contact-header"
-            onClick={() => {
-              setEmailSent({ sent: true, successfull: true });
-            }}
-          >
-            Get in touch
-          </h2>
-          <p className="home-contact-lead">You can contact me for a project proposition or for any questions</p>
-          <div className="home-contact-media-container">
-            <div className="home-contact-media flex">
-              <i className="fas fa-phone-alt"></i>
-              <p>07498 610586</p>
-            </div>
-            <div className="home-contact-media flex">
-              <i className="fas fa-envelope"></i>
-              <a href="mailto:leonmichalak6@gmail.com">leonmichalak6@gmail.com</a>
+    <>
+      <section className="home-contact flex" id="contact">
+        <div className="home-contact-container flex">
+          <div className="home-contact-text flex">
+            <h2 className="home-contact-header">Get in touch</h2>
+            <p className="home-contact-lead">You can contact me for a project proposition or for any questions</p>
+            <div className="home-contact-media-container">
+              <div className="home-contact-media flex">
+                <i className="fas fa-phone-alt"></i>
+                <p>07498 610586</p>
+              </div>
+              <div className="home-contact-media flex">
+                <i className="fas fa-envelope"></i>
+                <a href="mailto:leonmichalak6@gmail.com">leonmichalak6@gmail.com</a>
+              </div>
             </div>
           </div>
-        </div>
-        <form className="home-contact-form">
-          <div className="home-contact-form-top flex">
+          <form className="home-contact-form">
+            <div className="home-contact-form-top flex">
+              <div className="home-contact-input-wrapper flex">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  required
+                  placeholder="John Doe"
+                  value={name}
+                  onChange={(e) => validateName(e.target.value)}
+                  style={errors.name !== "default" ? { borderBottom: "2px solid #FF0001" } : {}}
+                />
+                <p className="error-text" style={errors.name === "default" ? { visibility: "hidden" } : {}}>
+                  {errors.name}
+                </p>
+              </div>
+              <div className="home-contact-input-wrapper flex">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  required
+                  placeholder="Example@email.com"
+                  value={email}
+                  onChange={(e) => validateEmail(e.target.value)}
+                  style={errors.email !== "default" ? { borderBottom: "2px solid #FF0001" } : {}}
+                />
+                <p className="error-text" style={errors.email === "default" ? { visibility: "hidden" } : {}}>
+                  {errors.email}
+                </p>
+              </div>
+            </div>
             <div className="home-contact-input-wrapper flex">
-              <label htmlFor="name">Name</label>
+              <label htmlFor="message">Message</label>
               <input
                 type="text"
-                name="name"
-                id="name"
+                name="message"
+                id="message"
                 required
-                placeholder="John Doe"
-                value={name}
-                onChange={(e) => validateName(e.target.value)}
-                style={errors.name !== "default" ? { borderBottom: "2px solid #FF0001" } : {}}
+                placeholder="Hi there..."
+                value={message}
+                onChange={(e) => validateMessage(e.target.value)}
+                style={errors.message !== "default" ? { borderBottom: "2px solid #FF0001" } : {}}
               />
-              <p className="error-text" style={errors.name === "default" ? { visibility: "hidden" } : {}}>
-                {errors.name}
+              <p className="error-text" style={errors.message === "default" ? { visibility: "hidden" } : {}}>
+                {errors.message}
               </p>
             </div>
-            <div className="home-contact-input-wrapper flex">
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                required
-                placeholder="Example@email.com"
-                value={email}
-                onChange={(e) => validateEmail(e.target.value)}
-                style={errors.email !== "default" ? { borderBottom: "2px solid #FF0001" } : {}}
-              />
-              <p className="error-text" style={errors.email === "default" ? { visibility: "hidden" } : {}}>
-                {errors.email}
-              </p>
-            </div>
-          </div>
-          <div className="home-contact-input-wrapper flex">
-            <label htmlFor="message">Message</label>
-            <input
-              type="text"
-              name="message"
-              id="message"
-              required
-              placeholder="Hi there..."
-              value={message}
-              onChange={(e) => validateMessage(e.target.value)}
-              style={errors.message !== "default" ? { borderBottom: "2px solid #FF0001" } : {}}
-            />
-            <p className="error-text" style={errors.message === "default" ? { visibility: "hidden" } : {}}>
-              {errors.message}
-            </p>
-          </div>
-          <Button className="flex" type="submit" onClick={(e) => submitForm(e)}>
-            {/* <i className="fas fa-phone-alt"></i> */}
-            <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M29.1668 5.83325H8.75016C7.14162 5.83325 5.8335 7.14138 5.8335 8.74992V16.0416H8.75016V11.6666L18.0835 18.6666C18.336 18.8557 18.643 18.9579 18.9585 18.9579C19.274 18.9579 19.581 18.8557 19.8335 18.6666L29.1668 11.6666V24.7916H17.5002V27.7083H29.1668C30.7754 27.7083 32.0835 26.4001 32.0835 24.7916V8.74992C32.0835 7.14138 30.7754 5.83325 29.1668 5.83325ZM18.9585 15.677L9.72141 8.74992H28.1956L18.9585 15.677Z"
-                fill="white"
-              />
-              <path
-                d="M2.9165 17.5H13.1248V20.4167H2.9165V17.5ZM5.83317 21.875H14.5832V24.7917H5.83317V21.875ZM10.2082 26.25H16.0415V29.1667H10.2082V26.25Z"
-                fill="white"
-              />
-            </svg>
+            <Button className="flex" type="submit" onClick={(e) => submitForm(e)}>
+              {/* <i className="fas fa-phone-alt"></i> */}
+              <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M29.1668 5.83325H8.75016C7.14162 5.83325 5.8335 7.14138 5.8335 8.74992V16.0416H8.75016V11.6666L18.0835 18.6666C18.336 18.8557 18.643 18.9579 18.9585 18.9579C19.274 18.9579 19.581 18.8557 19.8335 18.6666L29.1668 11.6666V24.7916H17.5002V27.7083H29.1668C30.7754 27.7083 32.0835 26.4001 32.0835 24.7916V8.74992C32.0835 7.14138 30.7754 5.83325 29.1668 5.83325ZM18.9585 15.677L9.72141 8.74992H28.1956L18.9585 15.677Z"
+                  fill="white"
+                />
+                <path
+                  d="M2.9165 17.5H13.1248V20.4167H2.9165V17.5ZM5.83317 21.875H14.5832V24.7917H5.83317V21.875ZM10.2082 26.25H16.0415V29.1667H10.2082V26.25Z"
+                  fill="white"
+                />
+              </svg>
 
-            <p>Send mail</p>
-          </Button>
-        </form>
-      </div>
-      <div className="home-contact-bg"></div>
-      <div className="home-contact-wave"></div>
-    </section>
+              <p>
+                {emailStatus.recieved_response === undefined
+                  ? "Send mail"
+                  : emailStatus.recieved_response === false
+                  ? "Sending mail..."
+                  : "Email recieved"}
+              </p>
+            </Button>
+          </form>
+        </div>
+        <div className="home-contact-bg"></div>
+        <div className="home-contact-wave"></div>
+      </section>
+    </>
   );
 }
