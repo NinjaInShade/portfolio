a39d6ee26eb84d4500f523c6bb3b6cca5a3d78fe
Leon Michalak <leonmichalak6@gmail.com>
2021-08-19 19:44:09 +0100

    Hamburger icon button added, no sidebar menu yet though

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../assets/logo.png';
 import CV from '../assets/CV.pdf';
 import DownloadIcon from '../assets/icon-download.svg';
 import { Link } from 'gatsby';
 import '../styles/navbar.css';
+import '../styles/hamburger.css';
 
 export default function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  function toggleSidebar() {
+    setSidebar(!sidebar);
+  }
+
   return (
     <nav className='navbar'>
       <div className='container navbar-container'>
@@ -41,9 +48,22 @@ export default function Navbar() {
             </a>
           </li>
         </ul>
-        <a className='btn btn-primary' href='/#contact'>
+        <a className='btn btn-primary btn-navbar' href='/#contact'>
           Get in touch
         </a>
+        <button className='hamburger-btn' onClick={toggleSidebar} aria-haspopup='true'>
+          <svg className={`ham hamRotate ham8 ${sidebar && 'active'}`} viewBox='0 0 100 100' width='44'>
+            <path
+              class='line top'
+              d='m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20'
+            />
+            <path class='line middle' d='m 30,50 h 40' />
+            <path
+              class='line bottom'
+              d='m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20'
+            />
+          </svg>
+        </button>
       </div>
     </nav>
   );
