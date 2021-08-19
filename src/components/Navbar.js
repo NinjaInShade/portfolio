303a6b913f112a6a308e4598cae1c3d209ec4f17
Leon Michalak <leonmichalak6@gmail.com>
2021-08-19 20:18:56 +0100

    Added header copy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -54,12 +54,12 @@ export default function Navbar() {
         <button className='hamburger-btn' onClick={toggleSidebar} aria-haspopup='true'>
           <svg className={`ham hamRotate ham8 ${sidebar && 'active'}`} viewBox='0 0 100 100' width='44'>
             <path
-              class='line top'
+              className='line top'
               d='m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20'
             />
-            <path class='line middle' d='m 30,50 h 40' />
+            <path className='line middle' d='m 30,50 h 40' />
             <path
-              class='line bottom'
+              className='line bottom'
               d='m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20'
             />
           </svg>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import '../styles/header.css';
 
 export default function Homepage() {
   return (
     <Layout pageTitle='Leon Michalak | Home'>
-      <h1>Hello!</h1>
+      <header className='header'>
+        <div className='container'>
+          <h1 className='header-title'>
+            Frontend developer creating <span>unique websites</span>
+          </h1>
+          <p className='body-text header-description'>
+            Hi, I’m Leon, a web developer from the UK. I love working on creative projects that challenge myself.{' '}
+          </p>
+        </div>
+      </header>
     </Layout>
   );
 }
