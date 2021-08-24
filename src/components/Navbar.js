1fcaeb8a94af7ee556472d02e54768f340a510fb
Leon Michalak <leonmichalak6@gmail.com>
2021-08-24 17:14:58 +0100

    Updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import Logo from '../assets/logo.png';
+import Logo from '../assets/logo.svg';
 import CV from '../assets/CV.pdf';
-import DownloadIcon from '../assets/icon-download.svg';
 import { Link } from 'gatsby';
 import '../styles/navbar.css';
 import '../styles/hamburger.css';
@@ -43,7 +42,19 @@ export default function Navbar() {
           </li>
           <li className='navbar-list-item'>
             <a className='body-text navbar-download-link' href={CV} download>
-              <img src={DownloadIcon} alt='Download icon' className='navbar-download-icon' />
+              <svg
+                className='navbar-download-icon'
+                width='16'
+                height='16'
+                viewBox='0 0 16 16'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  d='M14 11V14H2V11H0V14C0 15.1 0.9 16 2 16H14C15.1 16 16 15.1 16 14V11H14ZM13 7L11.59 5.59L9 8.17V0H7V8.17L4.41 5.59L3 7L8 12L13 7Z'
+                  fill='hsla(209, 84%, 10%, 0.5)'
+                />
+              </svg>
               Download CV
             </a>
           </li>
