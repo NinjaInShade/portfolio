06b65e67b5a78fbe80c222a04688d7797193f2a5
Leon Michalak <leonmichalak6@gmail.com>
2021-08-26 17:59:02 +0100

    Added focus states on filter buttons

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -9,6 +9,7 @@ export default function Filter() {
     { name: 'CSS', active: false },
     { name: 'Javascript', active: false },
     { name: 'Landing Page', active: false },
+    { name: 'Multi Page', active: false },
     { name: 'Component', active: false },
     { name: 'Animation', active: false },
     { name: 'ReactJS', active: false },
