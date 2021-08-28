c95837c0809c43f2338a048f982cd32b6d976a8b
Leon Michalak <leonmichalak6@gmail.com>
2021-08-28 16:49:12 +0100

    Filter logic clean and working without any dependency issues etc

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,56 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { projects as projectsData } from '../projects-data';
+import React from 'react';
 import FilterTab from './FilterTab';
 import '../styles/filter.css';
 
-export default function Filter({ setProjects }) {
-  const [filterMode, setFilterMode] = useState('inclusive');
-  const [tabs, setTabs] = useState([
-    { name: 'HTML', active: false },
-    { name: 'CSS', active: false },
-    { name: 'Javascript', active: false },
-    { name: 'Landing Page', active: false },
-    { name: 'Full Page', active: false },
-    { name: 'Multi Page', active: false },
-    { name: 'Component', active: false },
-    { name: 'Animation', active: false },
-    { name: 'ReactJS', active: false },
-    { name: 'NodeJS', active: false },
-    { name: 'Full Stack', active: false },
-    { name: 'API', active: false },
-    { name: 'Database', active: false },
-  ]);
-
-  //TODO: Instead of relying on initial projects array, add a shown key to projects so we only have to work with loaded state (Mimicking if we had a database)
-  // Filter products
-  useEffect(() => {
-    // Filters only the active tabs, to lower case to avoid errors
-    const activeTabs = tabs.filter((tab) => tab.active === true).map((filteredTab) => filteredTab.name.toLowerCase());
-
-    // The original projects (not state) but the tags have been made to lower case to avoid errors
-    let updatedProjects = projectsData.map((project) => {
-      return { ...project, tabs: project.tabs.map((tab) => tab.toLowerCase()) };
-    });
-
-    if (activeTabs.length === 0) {
-      return setProjects(updatedProjects);
-    }
-
-    if (filterMode === 'inclusive') {
-      // We need to show only projects that have the active tabs
-
-      // Filter out projects to only the ones that have all active tabs
-      updatedProjects = updatedProjects.filter((project) => {
-        return activeTabs.every((tab) => {
-          return project.tabs.includes(tab);
-        });
-      });
-
-      // Updated projects state
-      setProjects(updatedProjects);
-    }
-  }, [tabs, filterMode, setProjects]);
-
+export default function Filter({ tabs, setTabs, filterMode, setFilterMode }) {
   function setFilterModeHandler(mode) {
     setFilterMode(mode);
   }
--- src/sections/ProjectsContainer.js
+++ src/sections/ProjectsContainer.js
@@ -7,6 +7,22 @@ import '../styles/projects.css';
 export default function ProjectsContainer() {
   const [projects, setProjects] = useState(projectsData);
   const [projectsShown, setProjectsShown] = useState(6);
+  const [filterMode, setFilterMode] = useState('inclusive');
+  const [tabs, setTabs] = useState([
+    { name: 'HTML', active: false },
+    { name: 'CSS', active: false },
+    { name: 'Javascript', active: false },
+    { name: 'Landing Page', active: false },
+    { name: 'Full Page', active: false },
+    { name: 'Multi Page', active: false },
+    { name: 'Component', active: false },
+    { name: 'Animation', active: false },
+    { name: 'ReactJS', active: false },
+    { name: 'NodeJS', active: false },
+    { name: 'Full Stack', active: false },
+    { name: 'API', active: false },
+    { name: 'Database', active: false },
+  ]);
 
   function loadMoreProjectsHandler() {
     if (projects.length <= projectsShown) {
@@ -18,12 +34,68 @@ export default function ProjectsContainer() {
 
   return (
     <>
-      <Filter setProjects={setProjects} />
+      <Filter
+        projects={projects}
+        setProjects={setProjects}
+        tabs={tabs}
+        setTabs={setTabs}
+        filterMode={filterMode}
+        setFilterMode={setFilterMode}
+      />
 
       <section className='projects' id='projects'>
         <div className='container'>
           <ul className='projects-container'>
             {projects.slice(0, projectsShown).map((project, index) => {
+              // Tabs that are currently selected, transformed to lower case to avoid capitalization errors
+              const activeTabs = tabs
+                .filter((tab) => tab.active === true)
+                .map((filteredTab) => filteredTab.name.toLowerCase());
+              // The tabs for the current project,  transformed to lower case to avoid capitalization errors
+              const projectTabs = project.tabs.map((tab) => tab.toLowerCase());
+
+              // Does the project include all active tabs (for inclusive)
+              const doesIncludeTabs = activeTabs.every((tab) => projectTabs.includes(tab));
+
+              if (activeTabs.length === 0) {
+                return (
+                  <li key={index}>
+                    <ProjectCard
+                      imgSrc={project.imgSrc}
+                      imgAlt={project.imgAlt}
+                      gradientHue={project.gradientHue}
+                      gradientSaturation={project.gradientSaturation}
+                      title={project.title}
+                      description={project.description}
+                      github={project.github}
+                      liveSite={project.liveSite}
+                      tabs={project.tabs}
+                    />
+                  </li>
+                );
+              }
+
+              if (filterMode === 'inclusive') {
+                return (
+                  doesIncludeTabs && (
+                    <li key={index}>
+                      <ProjectCard
+                        imgSrc={project.imgSrc}
+                        imgAlt={project.imgAlt}
+                        gradientHue={project.gradientHue}
+                        gradientSaturation={project.gradientSaturation}
+                        title={project.title}
+                        description={project.description}
+                        github={project.github}
+                        liveSite={project.liveSite}
+                        tabs={project.tabs}
+                      />
+                    </li>
+                  )
+                );
+              }
+
+              // Mode is exclusive then (exact match)
               return (
                 <li key={index}>
                   <ProjectCard
