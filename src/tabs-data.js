dd7c8f88c6fc1ca211b2c22415e49dbb05f1be6f
Leon Michalak <leonmichalak6@gmail.com>
2021-08-29 12:58:02 +0100

    Removed full page tab

--- src/tabs-data.js
+++ src/tabs-data.js
@@ -3,7 +3,6 @@ export const tabs = [
   { name: 'CSS', active: false },
   { name: 'Javascript', active: false },
   { name: 'Landing Page', active: false },
-  { name: 'Full Page', active: false },
   { name: 'Multi Page', active: false },
   { name: 'Component', active: false },
   { name: 'Animation', active: false },
